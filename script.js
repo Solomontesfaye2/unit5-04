2512f46b9323201acf667e38e35534d8650ba629
Solomon Tesfaye <solomon.tesfaye@ocsbstudent.ca>
2021-03-17 17:21:41 +0000

    space

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
-document.getElementById('change-image').addEventListener('click', image)
-document.getElementById('change-text').addEventListener('click', text)
+document.getElementById('change-image') .addEventListener('click', image)
+document.getElementById('change-text')    .addEventListener('click', text)
 document.getElementById('hide').addEventListener('click', hide)
 document.getElementById('show').addEventListener('click', show)
-document.getElementById('Bg').addEventListener('click', background)
+document.getElementById('bg').addEventListener('click', background)
 
 function image () {
-document.getElementById('picture').src = "images/dog.jpg"
+  document.getElementById('picture').src = 'images/dog.jpg'
 }
 
 function text () {
-  document.getElementById('text').innerHTML="New Text"
+  document.getElementById('text').innerHTML = 'New Text'
 }
 
 function hide () {
-document.getElementById('text').style. display="none"
+  document.getElementById('text').style. display = 'none'
 }
 
 function show () {
-document.getElementById('text').style. display="block"
+  document.getElementById('text').style. display = 'block'
 }
+
 function background () {
-  document.body.style.backgroundColor="blue"
+   document.body.style.backgroundColor='blue'
 }
